ca46d9af557ec588effc6dee90d9863b73375130
ikonnikovilya <ikonnikovila426@gmail.com>
2026-06-01 15:25:57 +0300

    Delete tentacles_cyber_immune directory

--- tentacles_cyber_immune/einit/config.py
+++
@@ -1,8 +0,0 @@
-# Описание связей в системе
-# Кому разрешено отправлять запросы
-TOPOLOGY = {
-    "neural_interface": ["inhibitor", "ai_module"],
-    "ai_module": ["inhibitor"],
-    "inhibitor": ["drivers"],
-    "drivers": [] # Драйверы - конечная точка, они никуда не шлют запросы
-}
